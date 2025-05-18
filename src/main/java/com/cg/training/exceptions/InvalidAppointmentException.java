643b37e05dc76fb274c3afe8a20e2a53bbe3f6da
Sanjona Bhattacharjee <sanju.bae19@gmail.com>
2025-05-18 22:01:47 +0530

    Final push InvalidAppointmentException.java

--- src/main/java/com/cg/training/exceptions/InvalidAppointmentException.java
+++ src/main/java/com/cg/training/exceptions/InvalidAppointmentException.java
@@ -1,5 +1,23 @@
+
 package com.cg.training.exceptions;
 
-public class InvalidAppointmentException {
+/**
+ * This class represents a custom exception for handling invalid appointment-related scenarios.
+ * It is used to throw errors when an appointment cannot be booked or completed due to certain conditions.
+ * 
+ * 
+ * @author Sanjona Bhattacharjee and Rishiraj Ray
+ */
 
+public class InvalidAppointmentException extends RuntimeException {
+
+    /**
+     * Constructor to create an InvalidAppointmentException with a specific message.
+     * 
+     * @param message the detail message explaining the exception.
+     */
+    public InvalidAppointmentException(String message) {
+        super(message);
+    }
 }
+
